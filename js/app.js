d87e566ae82c2985d45892f6cacc6f1e73ee8837
Sparklebunny Stevens <jeribeth@gmail.com>
2014-04-28 21:26:25 -0600

    Improved Javascript
    question text must be entered to receive response

--- js/app.js
+++ js/app.js
@@ -1,25 +1,25 @@
 $(document).ready(function() {
 
-	var responses = ["it is certain", 
-		"it is decidedly so", 
-		"without a doubt", 
-		"yes, definitely",
-		"you may rely on it",
-		"as I see it, yes",
-		"most likely",
-		"outlook good",
-		"yes",
-		"signs point to yes",
-		"reply hazy, try again",
-		"ask again later",
-		"better not tell you now",
-		"cannot predict now",
-		"concentrate and ask again",
-		"don't count on it",
-		"my reply is no",
-		"my sources say no",
-		"outlook not good",
-		"very doubtful"]
+	var responses = ["It is certain.", 
+		"It is decidedly so.", 
+		"Without a doubt.", 
+		"Yes, definitely.",
+		"You may rely on it.",
+		"As I see it, yes.",
+		"Most likely.",
+		"Outlook good!",
+		"Yes!",
+		"Signs point to yes!",
+		"Reply hazy, try again!",
+		"Ask again later.",
+		"Better not tell you now.",
+		"Cannot predict now.",
+		"Concentrate and ask again.",
+		"Don't count on it.",
+		"My reply is no.",
+		"My sources say no way.",
+		"Outlook not good.",
+		"Very doubtful."]
 
 	var newGame = 0
 
@@ -31,10 +31,16 @@ $(document).ready(function() {
 
 	
 	function processQuestion() {
-		var random = Math.floor(Math.random() * responses.length);
-		var answer = responses[random];
-		// alert(answer);
-		$("#answertext").append('<p>' + answer + '</p>');
+		if ($("#question").val().length >= 3) {
+			var random = Math.floor(Math.random() * responses.length);
+			var answer = responses[random];
+			// alert(answer);
+			$("#answertext").append('<p>' + answer + '</p>');
+		}
+		else {
+			alert("Please enter a question.");
+		}	
+		
 	};
 
 	$("#question").keypress(function(e) {
